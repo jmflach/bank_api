4b6ccd33670cac43806177e37bf396a6b8c48597
joao <joaoflach@gmail.com>
2022-07-19 16:45:48 -0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+joao = BankAccount.create!(name: 'joao', cpf: '098765432456', birthday: '23/23/1212')
+fred = BankAccount.create!(name: 'frederico', cpf: '04587210254', birthday: '15/04/2000')
+
+trans_1 = joao.transactions.create!(trans_type: "credit", amount: 500)
+trans_2 = joao.transactions.create!(trans_type: "debit", amount: 100)
+trans_3 = joao.transactions.create!(trans_type: "debit", amount: 100)
+
+trans_4 = fred.transactions.create!(trans_type: "credit", amount: 500)
+trans_5 = fred.transactions.create!(trans_type: "debit", amount: 1000)
+trans_6 = fred.transactions.create!(trans_type: "credit", amount: 100)
