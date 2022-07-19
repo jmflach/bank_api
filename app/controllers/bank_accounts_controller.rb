725ea868d93b3866fe20f0ae5c5eae8af98b00c9
joao <joaoflach@gmail.com>
2022-07-19 17:24:09 -0300

    added the create bank account end point

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,3 +1,26 @@
 class BankAccountsController < JSONAPI::ResourceController
   skip_before_action :verify_authenticity_token
+
+  def create
+      @acc = BankAccount.new(bank_param)
+
+      @acc.balance = 0
+
+
+
+      if @acc.save
+        @number = @acc.id.to_s
+        @zeroes = "000000000000"
+        @padding = @zeroes.truncate(@zeroes.length-@number.length, omission: '')
+        @acc.account_number = @padding + @number
+        render json: {"account_number": @acc.account_number}, status: 200
+      else
+         render json: {"error": "coundn't create account"}
+      end
+   end
+
+   def bank_param
+      # params.require(:client).permit(:name, :cpf, :birthday, :image)
+      params.permit(:name, :cpf, :birthday)
+   end
 end
